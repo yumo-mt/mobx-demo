c81aa661095277b4e00e23e22f310adca9f66260
荣长海 <rongchanghai@meituan.com>
2021-10-08 16:08:12 +0800

    init

--- src/view/PageA/store.js
+++ src/view/PageA/store.js
@@ -1,35 +1,6 @@
 import { observable } from '../../mobx-source/mobx';
 
-// configure({
-//     useProxies: "never"
-// })
-
-
-// var user = {
-//   income: 3,
-//   name: '张三'
-// };
-// var bankUser = observable.object(user);
-// var bankUser2 = observable.box(user);
-
-// console.log(bankUser);
-// console.log(bankUser2);
-
-
-// var pr1 = observable.box(2);
-// console.log(pr1);
-// console.log('--------华丽分割-----------')
-// var pr2 = observable('ddd');
-// console.log(pr2);
-
-
-
-// class Store {
-//   @observable data = 'text';
-// }
-
-// export default new Store();
-
+ 
 var parent = {
   inProp: {
     name: 'tony',
@@ -40,6 +11,4 @@ var parent = {
 debugger
 var bankUser = observable(parent);
 
-console.log(bankUser)
-console.log(bankUser.Symbol)
 export default bankUser;
