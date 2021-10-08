02d51a338a728e2f281c0d5a5bb3e4240e6d455f
荣长海 <rongchanghai@meituan.com>
2021-10-08 14:42:53 +0800

    init

--- src/view/PageA/index.js
+++ src/view/PageA/index.js
@@ -1,20 +1,17 @@
 import React,{useState} from 'react';
-import Store from './store';
+import  bankUser from './store';
 import { observer } from 'mobx-react'
 
 
 
 const PageA = observer(()=>{
-  // 使用的时候new一个新的实例，保证每次store都是初始状态。
-  const [store] = useState(()=>{
-  	return new Store;
-  })
+   
   const change = ()=>{
-      store.job = 'python工程师'
+    bankUser.job = 'python工程师'
   }
   return (
   	<>
-     <p>{store.job}</p>
+     <p>{bankUser.job}</p>
      <button onClick={change}>改变</button>
     </>
   )
--- src/view/PageA/store.js
+++ src/view/PageA/store.js
@@ -1,14 +1,26 @@
-import { configure,makeAutoObservable } from '../../mobx-source/mobx';
+import { configure,makeAutoObservable,observable } from '../../mobx-source/mobx';
 
-configure({
-  enforceActions: 'always',
-})
-class Store{
-  constructor() {
-    // !!! 必写的
-    makeAutoObservable(this)
-  }
-  job="前端"
+// configure({
+//   enforceActions: 'always',
+// })
+// class Store{
+//   constructor() {
+//     // !!! 必写的
+//     makeAutoObservable(this)
+//   }
+//   job="前端"
+// }
+
+// export default Store;
+
+var parent = {
+  inProp: {
+    name: 'tony',
+    age: 15
+  },
+  job: '钢铁侠'
 }
+debugger
+var bankUser = observable(parent);
 
-export default Store;
+export default bankUser;
